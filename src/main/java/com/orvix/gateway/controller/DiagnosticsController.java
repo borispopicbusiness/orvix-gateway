b9f12f8f8185dafde83a4cc21b212d323fa1d76d
Boris Popic <borispopic.business@gmail.com>
2025-11-20 13:17:00 +0100

    OX-20 Enable Kubernetes discovery #comment The task is ready. #time 1h

---
+++ src/main/java/com/orvix/gateway/controller/DiagnosticsController.java
@@ -0,0 +1,32 @@
+package com.orvix.gateway.controller;
+
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/diagnostics")
+public class DiagnosticsController {
+    private final DiscoveryClient discoveryClient;
+
+    public DiagnosticsController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+    @ApiResponse(responseCode = "200",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = String.class))
+            )
+    )
+    @GetMapping("/cloud/service/test")
+    public List<String> getCloudDiagnostics() {
+        return discoveryClient.getServices();
+    }
+}
