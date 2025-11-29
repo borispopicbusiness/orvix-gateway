d341beb3972287db34bbbb503067b179d440af60
Boris Popic <139922922+borispopicbusiness@users.noreply.github.com>
2025-11-29 14:25:19 +0100

    OX-26 Enable Spring WebFlux Security #comment Added the dependency an… (#5)
    * OX-26 Enable Spring WebFlux Security #comment Added the dependency and implemented a basic SecurityWebFilterChain that currently permits all requests. #time 1h
    
    * Code review issues are resolved.

---
+++ src/main/java/com/orvix/gateway/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.orvix.gateway.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain securityWebFluxFilterChain(ServerHttpSecurity http) {
+         http
+             .csrf(ServerHttpSecurity.CsrfSpec::disable)
+             .authorizeExchange(auth -> auth
+                     .pathMatchers("/actuator/**", "/public/**").permitAll()
+                     .anyExchange().authenticated()
+             )
+             .httpBasic(Customizer.withDefaults());
+
+         return http.build();
+    }
+}
