8c329de3bda873d3013a30b4ddc25d8ca893c396
Boris Popic <borispopic.business@gmail.com>
2025-12-06 19:46:16 +0100

    OX-22 #comment Import removed

--- src/main/java/com/orvix/gateway/controller/DiagnosticsController.java
+++ src/main/java/com/orvix/gateway/controller/DiagnosticsController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
