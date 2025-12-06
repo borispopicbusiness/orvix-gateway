d89f30a09f10e48673d2fd96121421a9ba00d594
Boris Popic <borispopic.business@gmail.com>
2025-12-06 17:52:11 +0100

    OX-22 Load ConfigMap into Gateway Microservice #comment Loading of config maps into Gateway works automatically now. It took me some time to stabilize the Spring Cloud Kubernetes loading and refreshing procedure. #time 5h #review

--- src/main/java/com/orvix/gateway/controller/DiagnosticsController.java
+++ src/main/java/com/orvix/gateway/controller/DiagnosticsController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/v1/diagnostics")
 public class DiagnosticsController {
